fe0b9baea9c0eba75fd2eb1880b32d53b064fd0a
ubuntu <ubuntu@ubuntu.myguest.virtualbox.org>
2024-11-17 00:34:34 +0300

    a

--- src/db_models.py
+++ src/db_models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, ForeignKey
+from sqlalchemy import create_engine, MetaData, UniqueConstraint, Column, Integer, String, ForeignKey
 from sqlalchemy.orm import mapped_column
 from .config import conf
 from . import db
@@ -6,19 +6,21 @@ from . import db
 class Users(db.Model):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    name = Column(String)
-    surname = Column(String)
+    name = Column(String, nullable=False)
+    surname = Column(String, nullable=False)
     patronymic = Column(String)
-    login = Column(String)
-    password_hash = Column(String)
+    login = Column(String, nullable=False)
+    password_hash = Column(String, nullable=False)
+    __table_args__ = (UniqueConstraint('login'), )
 
 class Papers(db.Model):
     __tablename__ = 'papers'
     id = Column(Integer, primary_key=True)
-    title = Column(String)
+    title = Column(String, nullable=False)
     link_ru = Column(String)
     link_en = Column(String)
-    filepath = Column(String)
+    filepath = Column(String, nullable=False)
+    __table_args__ = (UniqueConstraint('title', 'link_ru', 'link_en', 'filepath'), )
 
 class Favourites(db.Model):
     __tablename__ = 'favourites'
--- src/routes.py
+++ src/routes.py
@@ -4,28 +4,28 @@ from sqlalchemy import select
 from sqlalchemy import create_engine
 from flask import current_app as app
 
+import hashlib
 import jwt
 import datetime
 
 from .db_models import Users
 from .config import conf
 
-engine = create_engine(conf.postgres_url)
-
-def get_users():
-    Session = sessionmaker(engine)
-    with Session() as session:
-        result = session.execute(select(Users))
-    return result.all()
-
+engine = create_engine(conf.postgres_url)    
 
 # Секретный ключ для подписи JWT
 SECRET_KEY = 'your_secret_key'
 
+
+def hash_f(password):
+    md5_hash = hashlib.new('md5')
+    md5_hash.update(password.encode())
+    return md5_hash.hexdigest()
+
 # Функция для создания JWT
-def create_token(username):
+def create_token(login):
     payload = {
-        'username': username,
+        'login': login,
         'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1)  # Токен действителен 1 час
     }
     token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
@@ -35,7 +35,7 @@ def create_token(username):
 def verify_token(token):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
-        return payload['username']
+        return payload['login']
     except jwt.ExpiredSignatureError:
         return 'Token expired. Please log in again.'
     except jwt.InvalidTokenError:
@@ -44,14 +44,17 @@ def verify_token(token):
 # Эндпоинт для аутентификации
 @app.route('/login', methods=['POST'])
 def login():
-    username = request.json.get('username')
+    login = request.json.get('login')
     password = request.json.get('password')
 
-    users = get_users()
+    Session = sessionmaker(engine)
+    with Session() as session:
+        user = session.execute(select(Users).where(Users.login == login)).scalar()
+        if not user:
+            return jsonify({'message': 'User not found'}), 404
 
-    for user in users: 
-        if user.name == username and user.password == password:
-            token = create_token(username)
+        if user.password_hash == hash_f(password):
+            token = create_token(login)
             return jsonify({'token': token})
     return jsonify({'message': 'Invalid credentials'}), 401
 
@@ -62,8 +65,85 @@ def protected():
     if not token:
         return jsonify({'message': 'Token is missing'}), 403
 
-    username = verify_token(token)
-    if isinstance(username, str) and 'Token' in username:
-        return jsonify({'message': username}), 403
+    login = verify_token(token)
+    if isinstance(login, str) and 'Token' in login:
+        return jsonify({'message': login}), 403
 
-    return jsonify({'message': f'Hello, {username}! This is a protected route.'})
+    return jsonify({'message': f'Hello, {login}! This is a protected route.'})
+
+# Эндпоинт для создания пользователя
+@app.route('/register', methods=['POST'])
+def register():
+    name = request.json.get('name')
+    password = request.json.get('password')
+    surname = request.json.get('surname')
+    patronymic = request.json.get('patronymic')
+    login = request.json.get('login')
+    
+    if not login or not password:
+        return jsonify({'message': 'Username and password are required'}), 400
+
+    Session = sessionmaker(engine)
+    with Session() as session:
+        existing_user = session.execute(select(Users).where(Users.login == login)).scalar()
+        if existing_user:
+            return jsonify({'message': 'User already exists'}), 409
+
+        new_user = Users(
+            name=name,
+            login=login, 
+            surname=surname,
+            patronymic=patronymic,
+            password_hash=hash_f(password))
+        session.add(new_user)
+        session.commit()
+
+    return jsonify({'message': 'User created successfully'}), 201
+
+# Эндпоинт для изменения пароля
+@app.route('/change_password', methods=['POST'])
+def change_password():
+    token = request.headers.get('Authorization')
+    if not token:
+        return jsonify({'message': 'Token is missing'}), 403
+
+    login = verify_token(token)
+    if isinstance(login, str) and 'Token' in login:
+        return jsonify({'message': login}), 403
+
+    new_password = request.json.get('new_password')
+    if not new_password:
+        return jsonify({'message': 'New password is required'}), 400
+
+    Session = sessionmaker(engine)
+    with Session() as session:
+        user = session.execute(select(Users).where(Users.login == login)).scalar()
+        if not user:
+            return jsonify({'message': 'User not found'}), 404
+
+        user.password_hash = hash_f(new_password)
+        session.commit()
+
+    return jsonify({'message': 'Password changed successfully'}), 200
+
+# Эндпоинт для удаления пользователя
+@app.route('/delete_user', methods=['DELETE'])
+def delete_user():
+    token = request.headers.get('Authorization')
+    if not token:
+        return jsonify({'message': 'Token is missing'}), 403
+
+    login = verify_token(token)
+    if isinstance(login, str) and 'Token' in login:
+        return jsonify({'message': login}), 403
+
+    Session = sessionmaker(engine)
+    with Session() as session:
+        user = session.execute(select(Users).where(Users.login == login)).scalar()
+        if not user:
+            return jsonify({'message': 'User not found'}), 404
+
+        session.delete(user)
+        session.commit()
+
+    return jsonify({'message': 'User deleted successfully'}), 200
