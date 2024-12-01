425e3443d588905291c3ebb8a2b04048e6ac31b3
ubuntu <ubuntu@ubuntu.myguest.virtualbox.org>
2024-12-01 18:50:35 +0300

    follow to bind back and front

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -11,6 +11,18 @@ def create_app():
     """Construct the core application."""
     app = Flask(__name__,)
     app.config["SQLALCHEMY_DATABASE_URI"] = conf.postgres_url
+    
+    def add_cors_headers(response):
+        response.headers['Access-Control-Allow-Origin'] = '*'  # Adjust as needed
+        response.headers['Access-Control-Allow-Credentials'] = 'true'
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+        return response
+    
+    @app.after_request
+    def after_request(response):
+        return add_cors_headers(response)
+    
     # Initialize Database Plugin
     db.init_app(app)
 
